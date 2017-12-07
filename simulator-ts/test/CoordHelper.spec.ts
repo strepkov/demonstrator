d834bba988ba699b564c8a29e96cca14cac2e2b5
eugen.strepkov <eugen.strepkov@gmail.com>
2017-12-07 21:10:43 +0100

    Added a test file to check Jasmine framework

---
+++ simulator-ts/test/CoordHelper.spec.ts
@@ -0,0 +1,8 @@
+import {CoordHelper} from '../rwth.visualisation/CoordHelper';
+
+describe('Our awesome test', () => {
+    it('tests something completely non-sense', () => {
+        let expected = 1;
+        expect(CoordHelper.test()).toEqual(expected);
+    });
+});
