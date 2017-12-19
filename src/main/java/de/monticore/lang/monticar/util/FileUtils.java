4cd34b45d91edb8c950ae6bdc813163cf3552609
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2017-12-20 00:23:44 +0100

    Add file utility class

---
+++ src/main/java/de/monticore/lang/monticar/util/FileUtils.java
@@ -0,0 +1,36 @@
+package de.monticore.lang.monticar.util;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class FileUtils {
+
+  /**
+   * Deletes the file or directory specified in the supplied {@code path}. Directories do not need
+   * to be empty. In case a directory is not empty, it will recursively delete the directory's
+   * content.
+   *
+   * @throws UncheckedIOException whenever the underlying delete operations throw a checked
+   * {@code IOException}
+   */
+  public static void delete(Path path) {
+    try (Stream<Path> fileStream = Files.walk(path)) {
+      fileStream
+          .sorted(Comparator.reverseOrder())
+          .forEach(p -> {
+            try {
+              Files.delete(p);
+            } catch (IOException e) {
+              throw new UncheckedIOException(e);
+            }
+          });
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+}
