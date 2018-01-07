c85d092d406670373ab801dc8e7f31749e5e0aec
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-01-07 06:49:50 +0100

    Fix armadillo url

--- install_armadillo.sh
+++ install_armadillo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install libblas-dev liblapack-dev
-wget http://sourceforge.net/projects/arma/files/armadillo-8.300.3.tar.gz
+wget http://sourceforge.net/projects/arma/files/armadillo-8.300.3.tar.xz
 tar -xvf armadillo-8.300.3.tar.gz
 cd armadillo-8.300.3
 cmake .
