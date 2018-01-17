0a8a18864cef9ce17dce281b3007de95186a9173
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-01-17 22:49:21 +0100

    Test coverage of car class was increased

--- simulator-ts/test/Car.spec.ts
+++ simulator-ts/test/Car.spec.ts
@@ -1,10 +1,42 @@
 import {Car} from "../rwth.visualisation/car/Car";
 
 describe('create instance of Car class', () => {
-    it('should return the initialized class', () => {
+
+    let car = new Car(0,0);
+
+    afterEach(function(){
         
-        let car = new Car(0,0);
+        car = null;
+        car = new Car(0,0);
+    });
+    
+    it('should return the initialized class', () => {
 
         expect(car).not.toBeNull();
     });
+
+    it('should return the initial position', () => {
+
+        expect(car.getPosition()).toEqual([0,0]);
+    });
+
+    it('should return the correct position', () => {
+
+        car.setPosition([1,1]);
+
+        expect(car.getPosition()).toEqual([1,1]);
+    });
+
+    it('should return the initial angle', () => {
+
+        expect(car.getDegree()).toEqual(0);
+    });
+
+    it('should return the correct angle', () => {
+
+        car.setDegree(10);
+
+        expect(car.getDegree()).toEqual(10);
+    });
+
 });
