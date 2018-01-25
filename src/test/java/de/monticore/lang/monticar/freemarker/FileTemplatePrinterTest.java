f5c9bd20148d906187ff339ce0a1b158487f225e
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-01-25 01:45:26 +0100

    Use TemporaryDirectoryExtension

--- src/test/java/de/monticore/lang/monticar/freemarker/FileTemplatePrinterTest.java
+++ src/test/java/de/monticore/lang/monticar/freemarker/FileTemplatePrinterTest.java
@@ -2,7 +2,7 @@ package de.monticore.lang.monticar.freemarker;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import de.monticore.lang.monticar.util.FileUtils;
+import de.monticore.lang.monticar.junit.TemporaryDirectoryExtension;
 import de.monticore.lang.monticar.util.TextFile;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -11,16 +11,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 class FileTemplatePrinterTest {
 
   private static final Path TEMPLATE_DIR = Paths.get("src/test/resources/freemarker");
   private static final String TEMPLATE = "test.ftl";
-  private static final Path OUT_FILE = Paths.get("target/freemarker/test.txt");
+  private static final String OUT_FILE_NAME = "test.txt";
   private static final Map<String, Object> EMPTY_DATA_MODEL = new HashMap<>();
   private static final String EXPECTED_GENERATED_STRING = "test";
 
@@ -35,18 +35,15 @@ class FileTemplatePrinterTest {
       template = templateFactory.getTemplate(TEMPLATE);
     }
 
-    @AfterEach
-    void cleanUp() {
-      FileUtils.delete(OUT_FILE);
-    }
-
     @Test
-    void shouldGenerateToFile() throws IOException, TemplateException {
-      FileTemplatePrinter templatePrinter = new FileTemplatePrinter(template, OUT_FILE);
+    @ExtendWith(TemporaryDirectoryExtension.class)
+    void shouldGenerateToFile(Path tempDir) throws IOException, TemplateException {
+      Path outFile = tempDir.resolve(OUT_FILE_NAME);
+      FileTemplatePrinter templatePrinter = new FileTemplatePrinter(template, outFile);
 
       templatePrinter.process(EMPTY_DATA_MODEL);
 
-      TextFile generatedFile = new TextFile(OUT_FILE);
+      TextFile generatedFile = new TextFile(outFile);
       assertThat(generatedFile.read()).isEqualTo(EXPECTED_GENERATED_STRING);
     }
   }
