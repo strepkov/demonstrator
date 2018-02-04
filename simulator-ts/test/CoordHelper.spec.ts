3fabb056d934299d9ed1bcc32a317b5c37365f04
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-02-04 20:00:55 +0100

    Updated test of CoordHelper class

--- simulator-ts/test/CoordHelper.spec.ts
+++ simulator-ts/test/CoordHelper.spec.ts
@@ -1,8 +1,15 @@
 import {CoordHelper} from '../rwth.visualisation/CoordHelper';
+import * as math from "../libs/math.js";
 
-describe('artificial test', () => {
-    it('should return 1', () => {
-        let expected = 1;
-        expect(CoordHelper.test()).toEqual(expected);
+describe('test CoordHelper\' functions', () => {
+
+    it('should return an intersection point', () => {
+        
+        expect(CoordHelper.getIntersectionLine(
+            [-50, -20], // point 1 of the wall
+            [50, -20], // point 2 of the wall
+            [0,0], // the sensor position
+            [0,1])) // direction of the sensor
+            .toEqual(math.matrix([0,-20]));
     });
 });
