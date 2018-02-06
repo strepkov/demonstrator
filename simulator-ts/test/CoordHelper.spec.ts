51ffaff714f950f3eff278dfa40927bb0b9f33e5
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-02-06 20:40:01 +0100

    Added new tests

--- simulator-ts/test/CoordHelper.spec.ts
+++ simulator-ts/test/CoordHelper.spec.ts
@@ -3,7 +3,7 @@ import * as math from "../libs/math.js";
 
 describe('test CoordHelper\' functions', () => {
 
-    it('should return an intersection point', () => {
+    it('should return an intersection point with line', () => {
         
         expect(CoordHelper.getIntersectionLine(
             [-50, -20], // point 1 of the wall
@@ -12,4 +12,28 @@ describe('test CoordHelper\' functions', () => {
             [0,1])) // direction of the sensor
             .toEqual([0,-20]);
     });
+
+    it('test getIntersectionCirclePlus()', () => {
+        
+        expect(CoordHelper.getIntersectionCirclePlus(
+            [0,0], // the sensor position [4.15, 1.85]
+            [1,0], // direction of the sensor FRONT_LEFT 
+            [123, 22], // Middle point
+            60, // radius
+            true // 1 or -1
+            )) 
+            .toEqual([67.17885705218855, 0]);
+    });
+
+    it('test getIntersectionCircle()', () => {
+        
+        expect(CoordHelper.getIntersectionCircle(
+            [0,0], // the sensor position [4.15, 1.85]
+            [1,0], // direction of the sensor FRONT_LEFT 
+            [123, 22], // Middle point
+            60, // radius
+            )) 
+            .toEqual([[67.17885705218855, 0], [178.82114294781144, 0]]);
+    });
+
 });
