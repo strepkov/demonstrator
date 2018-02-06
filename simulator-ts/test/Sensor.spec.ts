3bcd7c7c5112215c42395e029bf2b8e25bbe192d
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-02-06 09:56:50 +0100

    test commented

--- simulator-ts/test/Sensor.spec.ts
+++ simulator-ts/test/Sensor.spec.ts
@@ -1,6 +1,7 @@
 import {Sensor} from "../rwth.visualisation/car/Sensor";
 import {Car} from "../rwth.visualisation/car/Car";
 import {Orientation} from "../rwth.visualisation/coord/Orientation";
+import {WallLinear} from "../rwth.visualisation/track/WallLinear";
 import * as math from "../libs/math.js";
 
 describe('The sensors test', () => {
@@ -42,5 +43,16 @@ describe('The sensors test', () => {
         
         expect(sensor.getPosition(car)).toEqual(position);
     });
+
+
+    // it('should return GET POSITION', () => {
+
+    //     let car2 = new Car(60,0);
+    //     car2.setDegree(60);
+    //     let sensor = car2.getSensor(Orientation.FRONT_LEFT_SIDE);
+    //     let wall = new WallLinear([-120, -20], [122, -20])
+        
+    //     expect(sensor.getIntersections(wall, car2)).toEqual([[0,0],[0,0]]);
+    // });
 
 });
