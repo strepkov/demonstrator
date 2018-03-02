2b0de2719935282f15e808e43b81e80766bf81e0
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-03-02 01:23:11 +0100

    Modify tar command options

--- install_armadillo.sh
+++ install_armadillo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install libblas-dev liblapack-dev
 cd ./src/integration-test/resources
-tar -xvfJ armadillo-8.300.3.tar.xz
+tar -xJf armadillo-8.300.3.tar.xz
 cd armadillo-8.300.3
 cmake .
 make
