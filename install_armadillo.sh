f3f741a71081529e4c2f41ab8db38d6ca417a486
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-03-02 01:06:26 +0100

    Include armadillo in repository

--- install_armadillo.sh
+++ install_armadillo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install libblas-dev liblapack-dev
-wget http://sourceforge.net/projects/arma/files/armadillo-8.300.3.tar.xz
-tar -xvf armadillo-8.300.3.tar.xz
+cd ./src/integration-test/resources
+tar -xvfJ armadillo-8.300.3.tar.xz
 cd armadillo-8.300.3
 cmake .
 make
