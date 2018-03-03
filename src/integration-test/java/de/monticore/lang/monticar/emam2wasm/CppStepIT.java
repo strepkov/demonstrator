39d5f59ea90e76eb6c0b1635b115c6f7847e7e42
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-03-03 16:40:52 +0100

    Use sorted file stream in test assert

--- src/integration-test/java/de/monticore/lang/monticar/emam2wasm/CppStepIT.java
+++ src/integration-test/java/de/monticore/lang/monticar/emam2wasm/CppStepIT.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -87,7 +88,8 @@ class CppStepIT {
   }
 
   private List<Path> files(Path dir) throws IOException {
-    try (Stream<Path> fileStream = Files.walk(dir)) {
+    try (Stream<Path> fileStream = Files.walk(dir)
+        .sorted(Comparator.comparing(Path::getFileName))) {
       return fileStream.collect(Collectors.toList());
     }
   }
