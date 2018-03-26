7db88a5e26030f3be464937c36546dbb88bbf0c5
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-03-26 05:11:53 +0200

    Fix shell command for linux

--- src/test/java/de/monticore/lang/monticar/setup/action/SetupActionTest.java
+++ src/test/java/de/monticore/lang/monticar/setup/action/SetupActionTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 class SetupActionTest {
 
-  public static final String SUBDIR_NAME = "dir";
-  public static final String FILE_NAME = "file.txt";
+  private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
+  private static final String SUBDIR_NAME = "dir";
+  private static final String FILE_NAME = "file.txt";
+  private static final String WINDOWS = "windows";
+
+  private static <T> T[] concat(T[] first, T[] second) {
+    T[] result = Arrays.copyOf(first, first.length + second.length);
+    System.arraycopy(second, 0, result, first.length, second.length);
+    return result;
+  }
+
+  private String[] shell() {
+    return OS_NAME.startsWith(WINDOWS) ? new String[]{"cmd", "/C"}
+        : new String[]{"/bin/bash", "-c"};
+  }
 
   @Nested
   class WhenCommandFails {
@@ -27,8 +41,9 @@ class SetupActionTest {
       Path subdir = dir.resolve(SUBDIR_NAME);
       Files.createDirectory(subdir);
 
+      String[] command = concat(shell(), new String[]{"echo", "Hello", ">", SUBDIR_NAME});
       List<String[]> commands = new ArrayList<>();
-      commands.add(new String[]{"cmd", "/C", "echo", "Hello", ">", SUBDIR_NAME});
+      commands.add(command);
 
       SetupAction action = new SetupAction(dir, commands);
       assertThatExceptionOfType(ActionException.class).isThrownBy(action::execute);
@@ -41,8 +56,9 @@ class SetupActionTest {
     @Test
     @ExtendWith(TemporaryDirectoryExtension.class)
     void shouldCreateFile(Path dir) {
+      String[] command = concat(shell(), new String[]{"echo", "Hello", ">", FILE_NAME});
       List<String[]> commands = new ArrayList<>();
-      commands.add(new String[]{"cmd", "/C", "echo", "Hello", ">", FILE_NAME});
+      commands.add(command);
 
       SetupAction action = new SetupAction(dir, commands);
       action.execute();
