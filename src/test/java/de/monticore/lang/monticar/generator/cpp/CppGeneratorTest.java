b1ba670f470f105a9af64f99a7ae9fd8342ca3f4
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2018-04-02 15:34:40 +0200

    Remove tests for missing methods

--- src/test/java/de/monticore/lang/monticar/generator/cpp/CppGeneratorTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/cpp/CppGeneratorTest.java
@@ -1,7 +1,6 @@
 package de.monticore.lang.monticar.generator.cpp;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -9,8 +8,6 @@ import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.SourceDSL.strings;
 
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ExpandedComponentInstanceSymbol;
-import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.PortSymbol;
-import de.monticore.lang.monticar.contract.Precondition.PreconditionViolationException;
 import de.monticore.lang.monticar.freemarker.TemplateProcessor;
 import de.monticore.lang.monticar.resolver.Resolver;
 import de.monticore.lang.monticar.resolver.ResolverFactory;
@@ -18,7 +15,6 @@ import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,27 +24,8 @@ class CppGeneratorTest {
   private static final Path RESOLVING_BASE_DIR = Paths.get("src/test/resources/cppfilegeneration");
   private static final String ANY_MODEL = "models.scalarPort";
 
-  private static final PortSymbol NULL_PORT = null;
-  private static final String NULL_STRING = null;
-  private static final String EMPTY_STRING = "";
-  private static final String LOWERCASE_SINGLE_CHARACTER = "a";
-  private static final String UPPERCASE_SINGLE_CHARACTER = "A";
-  private static final String FIRST_LOWERCASE_STRING = "aSdF";
-  private static final String FIRST_UPPERCASE_STRING = "ASdF";
-
-  private static final Pattern VARIABLE_NAME_PATTERN = Pattern
-      .compile("^[a-zA-Z_$][a-zA-Z_$0-9]*$");
-  private static final int CODEPOINT_LOWERCASE_A = 97;
-  private static final int CODEPOINT_LOWERCASE_Z = 122;
-
-  private static final String NOT_ARRAY_PORT_NAME = "inport";
-
   private static final String ANY_FULL_NAME = "some.full.name";
   private static final String EXPECTED_CPP_CLASS_NAME = "some_full_name";
-
-  private boolean isValidVariableName(String name) {
-    return VARIABLE_NAME_PATTERN.matcher(name).matches();
-  }
 
   @Nested
   class Generate {
@@ -97,104 +74,6 @@ class CppGeneratorTest {
     }
   }
 
-  @Nested
-  class GetGetterMethodName {
 
-    @Nested
-    class ShouldThrowException {
-
-      @Test
-      void whenPortIsNull() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getGetterMethodName(NULL_PORT));
-      }
-
-      @Test
-      void whenPortNameIsNull() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getGetterMethodName(NULL_STRING));
-      }
-
-      @Test
-      void whenPortNameIsEmpty() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getGetterMethodName(EMPTY_STRING));
-      }
-    }
-
-    @Nested
-    class ShouldCapitalizeName {
-
-      @Test
-      void whenGivenLowercaseSingleCharacter() {
-        assertThat(CppGenerator.getGetterMethodName(LOWERCASE_SINGLE_CHARACTER)).isEqualTo("getA");
-      }
-
-      @Test
-      void whenGivenUppercaseSingleCharacter() {
-        assertThat(CppGenerator.getGetterMethodName(UPPERCASE_SINGLE_CHARACTER)).isEqualTo("getA");
-      }
-
-      @Test
-      void whenGivenFirstLowercaseString() {
-        assertThat(CppGenerator.getGetterMethodName(FIRST_LOWERCASE_STRING)).isEqualTo("getASdF");
-      }
-
-      @Test
-      void whenGivenFirstUppercaseString() {
-        assertThat(CppGenerator.getGetterMethodName(FIRST_UPPERCASE_STRING)).isEqualTo("getASdF");
-      }
-    }
-  }
-
-  @Nested
-  class GetSetterMethodName {
-
-    @Nested
-    class ShouldThrowException {
-
-      @Test
-      void whenPortIsNull() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getSetterMethodName(NULL_PORT));
-      }
-
-      @Test
-      void whenPortNameIsNull() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getSetterMethodName(NULL_STRING));
-      }
-
-      @Test
-      void whenPortNameIsEmpty() {
-        assertThatExceptionOfType(PreconditionViolationException.class)
-            .isThrownBy(() -> CppGenerator.getSetterMethodName(EMPTY_STRING));
-      }
-    }
-
-    @Nested
-    class ShouldCapitalizeName {
-
-      @Test
-      void whenGivenLowercaseSingleCharacter() {
-        assertThat(CppGenerator.getSetterMethodName(LOWERCASE_SINGLE_CHARACTER)).isEqualTo("setA");
-      }
-
-      @Test
-      void whenGivenUppercaseSingleCharacter() {
-        assertThat(CppGenerator.getSetterMethodName(UPPERCASE_SINGLE_CHARACTER)).isEqualTo("setA");
-      }
-
-      @Test
-      void whenGivenFirstLowercaseString() {
-        assertThat(CppGenerator.getSetterMethodName(FIRST_LOWERCASE_STRING)).isEqualTo("setASdF");
-      }
-
-      @Test
-      void whenGivenFirstUppercaseString() {
-        assertThat(CppGenerator.getSetterMethodName(FIRST_UPPERCASE_STRING)).isEqualTo("setASdF");
-      }
-    }
-  }
 
 }
