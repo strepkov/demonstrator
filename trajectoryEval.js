86e724b49564e35657cc9d34e55d0965409d4b5f
Ievgen Strepkov <eugen.strepkov@gmail.com>
2018-04-07 21:53:34 +0200

    Added track and over the track image drown the trace from telemety-log.json file

---
+++ trajectoryEval.js
@@ -0,0 +1,43 @@
+var jsonSimulationData;
+
+function readJSONFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                jsonSimulationData = JSON.parse(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readJSONFile("telemety-log.json");
+
+var distBetweenPoints = new Array();
+
+// Read all telemetry records from the file
+for(var i=0; i<jsonSimulationData.telemetry.length-1; i++){
+
+    // Add in the array changing position related to x and y
+    var diffX = jsonSimulationData.telemetry[i].Position[0] -
+                    jsonSimulationData.telemetry[i+1].Position[0];
+    var diffY = jsonSimulationData.telemetry[i].Position[1] -
+                    jsonSimulationData.telemetry[i+1].Position[1];
+
+    distBetweenPoints.push({
+        "x": diffX,
+        "y": diffY,
+        "v": {
+                // Add begin and end points of vector
+                "x0": jsonSimulationData.telemetry[i].Position[0],
+                "y0": jsonSimulationData.telemetry[i].Position[1],
+                "x1": jsonSimulationData.telemetry[i].Position[0]+diffX,
+                "y1": jsonSimulationData.telemetry[i].Position[1]+diffY
+            }
+    });
+}
+
+console.log(distBetweenPoints.length);
